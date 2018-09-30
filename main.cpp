4414a22331c3c2dc4ca5e556a3028bb5b36951c7
dsolodkiy <dsolodkiy@vniiftri.ru>
2018-09-30 13:22:48 +0300

    console output changed

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include "lib.h"
 
-#include <spdlog/spdlog.h>
+//#include <spdlog/spdlog.h>
 
 int main()
 {
-    auto logger = spdlog::stdout_logger_mt("console");
+    //auto logger = spdlog::stdout_logger_mt("console");
 
-    logger->info("build {}", version());
+    //logger->info("build {}", version());
+    std::cout << "build " << version() << std::endl;
     std::cout << "Hello, World!" << std::endl;
 
 
